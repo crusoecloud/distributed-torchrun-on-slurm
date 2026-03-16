52544eea62567bc6e9bb72074a8f293c0990b157
Abe Sharp <abe24seven@gmail.com>
2026-03-15 19:44:17 -0700

    Update venv_setup.sh

--- QWEN-VL-LoRA-SFT/venv_setup.sh
+++ QWEN-VL-LoRA-SFT/venv_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-uv --python 3.12 venv
+uv venv --python 3.12 
 source .venv/bin/activate
 uv pip install "https://github.com/lesj0610/flash-attention/releases/download/v2.8.3-cu12-torch2.10-cp312/flash_attn-2.8.3+cu12torch2.10cxx11abiTRUE-cp312-cp312-linux_x86_64.whl"
 uv pip install transformers deepspeed accelerate webdataset qwen-vl-utils sorted peft
